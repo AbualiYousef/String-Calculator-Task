cfe6559a5d3d0fe0526410e6312eab1b63ab7ce7
AbualiYousef <abualiyousef@outlook.com>
2024-07-10 16:35:22 +0300

    Update Tests module to ensure all test cases pass with the refactored code

--- StringCalculator/StringCalculator.Tests/Tests.fs
+++ StringCalculator/StringCalculator.Tests/Tests.fs
@@ -2,7 +2,9 @@ module Tests
 
 open Xunit
 open FsUnit.Xunit
-open Program
+open StringCalculator.Lib.StringCalculator
+open StringCalculator.Lib.Exceptions
+
 
 [<Theory>]
 [<InlineData("", 0)>]
@@ -11,20 +13,20 @@ open Program
 [<InlineData("5,6", 11)>]
 
 let ``Add should handle 0, 1, or 2 numbers`` (input: string, expected: int) =
-    Add input |> should equal expected
+    add input |> should equal expected
 
 [<Theory>]
 [<InlineData("1,2,3", 6)>]
 [<InlineData("1,2,3,4", 10)>]
 let ``Add should handle unknown amount of numbers`` (input: string, expected: int) =
-    Add input |> should equal expected
+    add input |> should equal expected
 
 [<Theory>]
 [<InlineData("1\n2,3", 6)>]
 [<InlineData("1\n2\n3", 6)>]
 [<InlineData("1,2\n3,4", 10)>]
 let ``Add should handle new lines between numbers`` (input: string, expected: int) =
-    Add input |> should equal expected
+    add input |> should equal expected
 
 [<Theory>]
 [<InlineData("//;\n1;2", 3)>]
@@ -32,13 +34,13 @@ let ``Add should handle new lines between numbers`` (input: string, expected: in
 [<InlineData("//.\n1.2.3", 6)>]
 [<InlineData("//\n\n1\n2\n3", 6)>]
 let ``Add should support different delimiters`` (input: string, expected: int) =
-    Add input |> should equal expected
+    add input |> should equal expected
 
 [<Theory>]
 [<InlineData("1,-2", "Negatives not allowed: -2")>]
 [<InlineData("1,-2,-3", "Negatives not allowed: -2,-3")>]
 let ``Add should throw exception for negative numbers`` (input: string, expectedMessage: string) =
-    let ex = Assert.Throws<NegativeNumberException>(fun () -> Add input |> ignore)
+    let ex = Assert.Throws<NegativeNumberException>(fun () -> add input |> ignore)
     ex.Message |> should equal expectedMessage
 
 [<Theory>]
@@ -46,7 +48,7 @@ let ``Add should throw exception for negative numbers`` (input: string, expected
 [<InlineData("1000,1001,2", 1002)>]
 [<InlineData("1000,0", 1000)>]
 let ``Add should ignore numbers bigger than 1000`` (input: string, expected: int) =
-    Add input |> should equal expected
+    add input |> should equal expected
 
 [<Theory>]
 [<InlineData("//[***]\n1***2***3", 6)>]
@@ -54,14 +56,14 @@ let ``Add should ignore numbers bigger than 1000`` (input: string, expected: int
 [<InlineData("//[$#:*]\n5$#:*6$#:*3", 14)>]
 
 let ``Add should support delimiters of any length`` (input: string, expected: int) =
-    Add input |> should equal expected
+    add input |> should equal expected
 
 [<Theory>]
 [<InlineData("//[*][%]\n1*2%3", 6)>]
 [<InlineData("//[*][%]\n1*2*3", 6)>]
 [<InlineData("//[*][%]\n1*2*3%4", 10)>]
 let ``Add should support multiple delimiters`` (input: string, expected: int) =
-    Add input |> should equal expected
+    add input |> should equal expected
 
 [<Theory>]
 [<InlineData("//[**][%%]\n1**2%%3", 6)>]
@@ -70,4 +72,4 @@ let ``Add should support multiple delimiters`` (input: string, expected: int) =
 [<InlineData("//[***][%%%]\n1***2%%%3", 6)>]
 [<InlineData("//[***][$$$$]\n1***2$$$$3", 6)>]
 let ``Add should support multiple delimiters with length longer than 1 char`` (input: string, expected: int) =
-    Add input |> should equal expected
+    add input |> should equal expected
