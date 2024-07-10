1adec4ae07cf2112c22c7bb7774bed89e1ab7425
AbualiYousef <abualiyousef@outlook.com>
2024-07-10 03:13:23 +0300

    Implement Step 2: Handle an unknown amount of numbers

--- StringCalculator/StringCalculator.App/Program.fs
+++ StringCalculator/StringCalculator.App/Program.fs
@@ -6,8 +6,5 @@ let Add (numbers: string) : int =
     if String.IsNullOrWhiteSpace(numbers) then
         0
     else
-        let splitNumbers = numbers.Split([|','|])
-        match splitNumbers.Length with
-        | 1 -> Int32.Parse(splitNumbers[0])
-        | 2 -> (Int32.Parse(splitNumbers[0]) + Int32.Parse(splitNumbers[1]))
-        | _ -> failwith "Invalid input"
+        let splitNumbers = numbers.Split([| ',' |])
+        splitNumbers |> Array.map Int32.Parse |> Array.sum
--- StringCalculator/StringCalculator.Tests/Tests.fs
+++ StringCalculator/StringCalculator.Tests/Tests.fs
@@ -12,3 +12,9 @@ open Program
 
 let ``Add should handle 0, 1, or 2 numbers`` (input: string, expected: int) =
     Add input |> should equal expected
+    
+[<Theory>]
+[<InlineData("1,2,3", 6)>]
+[<InlineData("1,2,3,4", 10)>]
+let ``Add should handle unknown amount of numbers`` (input: string, expected: int) =
+    Add input |> should equal expected
