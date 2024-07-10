9a0f1463cb345055507f0142f53c2afed8b9902d
AbualiYousef <abualiyousef@outlook.com>
2024-07-10 16:31:58 +0300

    Add NumberParser module to handle the parsing of numbers from the input string

---
+++ StringCalculator/StringCalculator.Lib/NumberParser.fs
@@ -0,0 +1,12 @@
+﻿namespace StringCalculator.Lib
+
+module NumberParser =
+
+    open System
+
+    let parseNumbers (numbers: string, delimiters: string[]) : int[] =
+        let splitNumbers = numbers.Split(delimiters, StringSplitOptions.None)
+
+        splitNumbers
+        |> Array.choose (fun n -> if n <> "" then Some(Int32.Parse(n)) else None)
+        |> Array.filter (fun n -> n <= 1000)
