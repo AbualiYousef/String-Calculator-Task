91eb56d36c4a11ad6167e999e12c6730e12dc6bb
AbualiYousef <abualiyousef@outlook.com>
2024-07-10 16:34:07 +0300

    Refactor StringCalculator module to use the new DelimiterFactory, NumberParser, and NumberValidator modules

---
+++ StringCalculator/StringCalculator.Lib/StringCalculator.fs
@@ -0,0 +1,16 @@
+﻿namespace StringCalculator.Lib
+
+open StringCalculator.Lib.DelimiterFactory
+open StringCalculator.Lib.NumberParser
+open StringCalculator.Lib.NumberValidator
+
+module StringCalculator =
+
+    let add (input: string) : int =
+        if System.String.IsNullOrWhiteSpace(input) then
+            0
+        else
+            let delimiters, numbers = getDelimitersAndNumbers input
+            let parsedNumbers = parseNumbers (numbers, delimiters)
+            validateNumbers parsedNumbers
+            parsedNumbers |> Array.sum
