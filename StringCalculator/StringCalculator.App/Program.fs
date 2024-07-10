e7c1b94cc79c21950aedf92c0ea3caa2ba582ba4
AbualiYousef <abualiyousef@outlook.com>
2024-07-10 18:15:04 +0300

    Handle InvalidDelimiterSequenceException in console application

--- StringCalculator/StringCalculator.App/Program.fs
+++ StringCalculator/StringCalculator.App/Program.fs
@@ -6,10 +6,13 @@ open StringCalculator.Lib.Exceptions
 let main argv =
     printfn "Enter a string of numbers to add (e.g., \"1,2,3\"): "
     let input = Console.ReadLine()
+
     try
         let result = add (input.Replace("\\n", "\n"))
         printfn $"The sum is: %d{result}"
     with
-    | :? NegativeNumberException as ex ->
-        printfn $"%s{ex.Message}"
+    | :? NegativeNumberException as ex -> printfn $"Error: %s{ex.Message}"
+    | :? InvalidDelimiterSequenceException as ex -> printfn $"Error: %s{ex.Message}"
+    | ex -> printfn $"Unhandled exception: %s{ex.Message}"
+
     0
