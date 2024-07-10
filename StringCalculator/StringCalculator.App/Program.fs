09880f2ffc8e09e6e6e26d9504ccdae270ff0125
AbualiYousef <abualiyousef@outlook.com>
2024-07-10 15:26:55 +0300

    Implement Steps 8 & 9: Support for multiple delimiters, including delimiters with length longer than one character

--- StringCalculator/StringCalculator.App/Program.fs
+++ StringCalculator/StringCalculator.App/Program.fs
@@ -16,19 +16,27 @@ let Add (numbers: string) : int =
                     ([| "\n" |], numbers.Substring(4))
                 else
                     let endIndex = numbers.IndexOf('\n')
-                    let delimiter = numbers.Substring(2, endIndex - 2)
-                    let actualDelimiter = 
-                        if delimiter.StartsWith("[") && delimiter.EndsWith("]") then
-                            delimiter.Trim([| '['; ']' |])
-                        else delimiter
-                    ([| actualDelimiter |], numbers.Substring(endIndex + 1))
+                    let delimiterPart = numbers.Substring(2, endIndex - 2)
+
+                    let delimiters =
+                        if delimiterPart.StartsWith("[") then
+                            delimiterPart.Split([| '['; ']' |], StringSplitOptions.RemoveEmptyEntries)
+                        else
+                            [| delimiterPart |]
+
+                    (delimiters, numbers.Substring(endIndex + 1))
             else
                 ([| ","; "\n" |], numbers)
 
         let splitNumbers = numbers.Split(delimiters, StringSplitOptions.None)
-        let parsedNumbers = splitNumbers |> Array.map Int32.Parse |> Array.filter (fun n -> n <= 1000)
+
+        let parsedNumbers =
+            splitNumbers |> Array.map Int32.Parse |> Array.filter (fun n -> n <= 1000)
+
         let negatives = parsedNumbers |> Array.filter (fun n -> n < 0)
+
         if negatives.Length > 0 then
             let message = negatives |> Array.map string |> String.concat ","
             raise (NegativeNumberException $"Negatives not allowed: %s{message}")
+
         parsedNumbers |> Array.sum
--- StringCalculator/StringCalculator.Tests/Tests.fs
+++ StringCalculator/StringCalculator.Tests/Tests.fs
@@ -12,20 +12,20 @@ open Program
 
 let ``Add should handle 0, 1, or 2 numbers`` (input: string, expected: int) =
     Add input |> should equal expected
-    
+
 [<Theory>]
 [<InlineData("1,2,3", 6)>]
 [<InlineData("1,2,3,4", 10)>]
 let ``Add should handle unknown amount of numbers`` (input: string, expected: int) =
     Add input |> should equal expected
-    
+
 [<Theory>]
 [<InlineData("1\n2,3", 6)>]
 [<InlineData("1\n2\n3", 6)>]
 [<InlineData("1,2\n3,4", 10)>]
 let ``Add should handle new lines between numbers`` (input: string, expected: int) =
     Add input |> should equal expected
-    
+
 [<Theory>]
 [<InlineData("//;\n1;2", 3)>]
 [<InlineData("//$\n1$2", 3)>]
@@ -33,25 +33,41 @@ let ``Add should handle new lines between numbers`` (input: string, expected: in
 [<InlineData("//\n\n1\n2\n3", 6)>]
 let ``Add should support different delimiters`` (input: string, expected: int) =
     Add input |> should equal expected
-    
+
 [<Theory>]
 [<InlineData("1,-2", "Negatives not allowed: -2")>]
 [<InlineData("1,-2,-3", "Negatives not allowed: -2,-3")>]
 let ``Add should throw exception for negative numbers`` (input: string, expectedMessage: string) =
     let ex = Assert.Throws<NegativeNumberException>(fun () -> Add input |> ignore)
     ex.Message |> should equal expectedMessage
-    
+
 [<Theory>]
 [<InlineData("2,1001", 2)>]
 [<InlineData("1000,1001,2", 1002)>]
 [<InlineData("1000,0", 1000)>]
 let ``Add should ignore numbers bigger than 1000`` (input: string, expected: int) =
     Add input |> should equal expected
-    
+
 [<Theory>]
 [<InlineData("//[***]\n1***2***3", 6)>]
 [<InlineData("//[abc]\n1abc2abc3", 6)>]
 [<InlineData("//[$#:*]\n5$#:*6$#:*3", 14)>]
 
 let ``Add should support delimiters of any length`` (input: string, expected: int) =
+    Add input |> should equal expected
+
+[<Theory>]
+[<InlineData("//[*][%]\n1*2%3", 6)>]
+[<InlineData("//[*][%]\n1*2*3", 6)>]
+[<InlineData("//[*][%]\n1*2*3%4", 10)>]
+let ``Add should support multiple delimiters`` (input: string, expected: int) =
+    Add input |> should equal expected
+
+[<Theory>]
+[<InlineData("//[**][%%]\n1**2%%3", 6)>]
+[<InlineData("//[**][%%]\n1**2**3", 6)>]
+[<InlineData("//[**][%%]\n1**2**3%%4", 10)>]
+[<InlineData("//[***][%%%]\n1***2%%%3", 6)>]
+[<InlineData("//[***][$$$$]\n1***2$$$$3", 6)>]
+let ``Add should support multiple delimiters with length longer than 1 char`` (input: string, expected: int) =
     Add input |> should equal expected
