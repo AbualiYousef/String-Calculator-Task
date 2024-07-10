4cad9c472547ba7b6ea656c7eae8e90965ff2115
AbualiYousef <abualiyousef@outlook.com>
2024-07-10 16:32:14 +0300

    Add NumberValidator module to handle validation of parsed numbers

---
+++ StringCalculator/StringCalculator.Lib/NumberValidator.fs
@@ -0,0 +1,11 @@
+﻿namespace StringCalculator.Lib
+
+open StringCalculator.Lib.Exceptions
+
+module NumberValidator =
+
+    let validateNumbers (numbers: int[]) =
+        let negatives = numbers |> Array.filter (fun n -> n < 0)
+        if negatives.Length > 0 then
+            let message = negatives |> Array.map string |> String.concat ","
+            raise (NegativeNumberException $"Negatives not allowed: %s{message}")
