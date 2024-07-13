17e1e8401649f3b98600d998038fa9d5e11b07c5
AbualiYousef <abualiyousef@outlook.com>
2024-07-13 19:24:37 +0300

    Add validateDelimitersInInput and refactor InputValidator

--- StringCalculator/StringCalculator.Lib/Validators/InputValidator.fs
+++ StringCalculator/StringCalculator.Lib/Validators/InputValidator.fs
@@ -3,10 +3,29 @@
 open StringCalculator.Lib.Exceptions
 
 module InputValidator =
-    let validateInput (input: string, delimiters: string[]) =
+
+    let validateInput (numbers: string, delimiters: string[]) =
         let invalidSequences =
             delimiters
             |> Array.collect (fun d1 -> delimiters |> Array.map (fun d2 -> d1 + d2))
 
-        if invalidSequences |> Array.exists input.Contains then
+        if invalidSequences |> Array.exists numbers.Contains then
             raise (InvalidDelimiterSequenceException("Invalid input: consecutive delimiters are not allowed"))
+
+    let validateDelimitersInInput (numbers: string, delimiters: string[]) =
+        let inputDelimiters =
+            numbers.ToCharArray()
+            |> Array.fold
+                (fun acc c ->
+                    if not (System.Char.IsDigit c) && c <> '-' then
+                        acc + string c
+                    else
+                        acc + " ")
+                ""
+            |> fun s -> s.Split([| ' ' |], System.StringSplitOptions.RemoveEmptyEntries)
+
+        let invalidDelimiter =
+            inputDelimiters |> Array.exists (fun d -> not (Array.contains d delimiters))
+
+        if invalidDelimiter then
+            raise (InvalidInputException("Invalid input: contains invalid delimiters"))
