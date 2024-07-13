1501be60c80ab02c74dc4a66c0b0577c69a0983d
AbualiYousef <abualiyousef@outlook.com>
2024-07-13 03:30:39 +0300

    Update InputValidator to simplify validation logic

--- StringCalculator/StringCalculator.Lib/Validators/InputValidator.fs
+++ StringCalculator/StringCalculator.Lib/Validators/InputValidator.fs
@@ -3,13 +3,10 @@
 open StringCalculator.Lib.Exceptions
 
 module InputValidator =
-
-    let private hasInvalidDelimiterSequence (input: string, delimiters: string[]) : bool =
-        delimiters |> Array.exists (fun delimiter -> input.Contains(delimiter + delimiter))
-
     let validateInput (input: string, delimiters: string[]) =
         let invalidSequences =
-            delimiters |> Array.collect (fun d1 -> delimiters |> Array.map (fun d2 -> d1 + d2))
+            delimiters
+            |> Array.collect (fun d1 -> delimiters |> Array.map (fun d2 -> d1 + d2))
 
         if invalidSequences |> Array.exists input.Contains then
             raise (InvalidDelimiterSequenceException("Invalid input: consecutive delimiters are not allowed"))
