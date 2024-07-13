c2f46a21708e0cdb410343d7017f4b6f0c0d26bd
AbualiYousef <abualiyousef@outlook.com>
2024-07-13 19:24:43 +0300

    Rename validateNumbers to validateNegativeNumbers and add filterNumbers

--- StringCalculator/StringCalculator.Lib/Validators/NumberValidator.fs
+++ StringCalculator/StringCalculator.Lib/Validators/NumberValidator.fs
@@ -4,9 +4,12 @@ open StringCalculator.Lib.Exceptions
 
 module NumberValidator =
 
-    let validateNumbers (numbers: int[]) =
+    let validateNegativeNumbers (numbers: int[]) =
         let negatives = numbers |> Array.filter (fun n -> n < 0)
 
         if negatives.Length > 0 then
             let message = negatives |> Array.map string |> String.concat ","
             raise (NegativeNumberException $"Negatives not allowed: %s{message}")
+
+    let filterNumbers (numbers: int[]) : int[] =
+        numbers |> Array.filter (fun n -> n <= 1000)
