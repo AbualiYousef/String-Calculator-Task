36fabcb6ce783a767c6273bc9be9e4483603a36a
AbualiYousef <abualiyousef@outlook.com>
2024-07-13 03:35:39 +0300

    Add DelimiterValidator module to validate malformed custom delimiters

---
+++ StringCalculator/StringCalculator.Lib/Validators/DelimiterValidator.fs
@@ -0,0 +1,12 @@
+﻿namespace StringCalculator.Lib.Validators
+
+open StringCalculator.Lib.Exceptions
+
+module DelimiterValidator =
+
+    let validateDelimiter (delimiterPart: string) =
+        if
+            (delimiterPart.StartsWith("[") && not (delimiterPart.EndsWith("]")))
+            || delimiterPart.StartsWith("]")
+        then
+            raise (InvalidInputException("Invalid input: malformed custom delimiter"))
