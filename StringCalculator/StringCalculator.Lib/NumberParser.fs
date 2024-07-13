99962e01c51c67ef414fc54264cec47192e87ca9
AbualiYousef <abualiyousef@outlook.com>
2024-07-13 19:25:21 +0300

    Refactor NumberParser to use RemoveEmptyEntries

--- StringCalculator/StringCalculator.Lib/NumberParser.fs
+++ StringCalculator/StringCalculator.Lib/NumberParser.fs
@@ -5,8 +5,5 @@ module NumberParser =
     open System
 
     let parseNumbers (numbers: string, delimiters: string[]) : int[] =
-        let splitNumbers = numbers.Split(delimiters, StringSplitOptions.None)
-
-        splitNumbers
-        |> Array.choose (fun n -> if n <> "" then Some(Int32.Parse(n)) else None)
-        |> Array.filter (fun n -> n <= 1000)
+        let splitNumbers = numbers.Split(delimiters, StringSplitOptions.RemoveEmptyEntries)
+        splitNumbers |> Array.map Int32.Parse
